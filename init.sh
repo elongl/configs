acaec964b54a52cf78d0217845cb486b9b68c4bf
Elon Gliksberg <elongliks@gmail.com>
2022-01-21 09:10:20 +0200

    Deleted VSCode from init script.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/bin
-echo "Created $(whoami)'s bin directory."
+echo "Created $USER\'s bin directory."
 
 ln -sf $PWD/gdbinit ~/.gdbinit
 echo "Linked GDB."
@@ -15,12 +15,6 @@ echo "Linked Nvim."
 
 ln -sf $PWD/tmux ~/.tmux.conf
 echo "Linked TMUX."
-
-ln -sf $PWD/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
-echo "Linked VSCode settings."
-
-cat vscode_extensions | xargs -L1 code --install-extension
-echo "Installed VSCode extensions."
 
 rm -rf ~/.ipython/profile_default/startup
 mkdir -p ~/.ipython/profile_default
